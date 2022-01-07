331513b127afa32f91587cf4a553442fba8858d4
Justyna Kubica-Ledzion <justynakubica-ledzion@Justynas-MacBook-Pro.local>
2022-01-07 13:38:57 +0100

    producer and consumer for 1 topic created

--- kafka-consumer/src/main/java/com/ledzion/kafkapoc/kafka/KafkaConsumer.java
+++ kafka-consumer/src/main/java/com/ledzion/kafkapoc/kafka/KafkaConsumer.java
@@ -1,15 +1,37 @@
 package com.ledzion.kafkapoc.kafka;
 
+import com.ledzion.kafkapoc.model.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "${kafka.topicName}", groupId = "${kafka.groupId}")
-    public void listen(String message) {
-        log.info("Received message: {}, in group HR", message);
+    @KafkaListener(
+            topicPartitions = @TopicPartition(
+                    topic="${kafka.topicName}",
+                    partitions = {"0"}),
+            groupId = "${kafka.groupId}")
+    public void listenPartition0(@Payload Employee employee,
+                                 @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                                 @Header(KafkaHeaders.OFFSET) int offset) {
+        log.info("Received message=[{}}] with offset=[{}}] to partition=[{}]", employee, offset, partition);
+    }
+
+    @KafkaListener(
+            topicPartitions = @TopicPartition(
+                    topic="${kafka.topicName}",
+                    partitions = {"1"}),
+            groupId = "${kafka.groupId}")
+    public void listenPartition1(@Payload Employee employee,
+                                 @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                                 @Header(KafkaHeaders.OFFSET) int offset) {
+        log.info("Received message=[{}}] with offset=[{}}] to partition=[{}]", employee, offset, partition);
     }
 }
--- kafka-producer/src/main/java/com/ledzion/kafkapoc/kafka/KafkaProducer.java
+++ kafka-producer/src/main/java/com/ledzion/kafkapoc/kafka/KafkaProducer.java
@@ -1,12 +1,15 @@
 package com.ledzion.kafkapoc.kafka;
 
 import com.ledzion.kafkapoc.model.Employee;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Component
 @Slf4j
@@ -14,17 +17,32 @@ public class KafkaProducer {
 
     private final String topicName;
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Employee> kafkaTemplate;
 
     @Autowired
-    public KafkaProducer(@Value(value = "${kafka.topicName}") String topicName,
-                         KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(@Value(value = "${kafka.topicName}") String topicName, KafkaTemplate<String, Employee> kafkaTemplate) {
         this.topicName = topicName;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendMessage(Employee employee) {
-        log.info("Sending message {} to Kafka topic: {}", employee, topicName);
-        kafkaTemplate.send(topicName, employee.toString());
+        log.info("Sending message [{}] to Kafka topic: {}", employee, topicName);
+
+        ListenableFuture<SendResult<String, Employee>> future = kafkaTemplate.send(topicName, employee);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+
+            @Override
+            public void onSuccess(SendResult<String, Employee> result) {
+                RecordMetadata recordMetadata = result.getRecordMetadata();
+                log.info("Sent message=[{}}] with offset=[{}}] to partition=[{}]", employee, recordMetadata.offset(),
+                        recordMetadata.partition());
+            }
+
+            @Override
+            public void onFailure(Throwable exception) {
+                log.info("Unable to send message=[{}}] due to: {}", employee, exception.getMessage());
+            }
+        });
     }
 }
